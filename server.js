97bf1b8bfbbb3dfbbd938abebcafd1c9529aa7e7
Jonathan Raitses <jonathan.raitses@gmail.com>
2019-01-19 16:22:05 -0500

    deployment time finished

--- server.js
+++ server.js
@@ -1,36 +1,12 @@
 const express = require("express");
-const request = require("request");
+const proxy = require("http-proxy-middleware");
 const app = express();
 
 app.use(express.static(__dirname));
 
-app.use((req, res, next) => {
-  let url = req.url.split("/");
-  let path = url.slice(2).join("/");
-  let endpoint = url[1];
-  if (endpoint === "main") {
-    let proxy =
-      "54.146.26.33/" + path;
-    request(proxy)
-      .on("error", err => console.log(err))
-      .pipe(res);
-  } else if (endpoint === "details") {
-    let proxy =
-      "18.217.66.137/" + path;
-
-    request(proxy)
-      .on("error", err => console.log(err))
-      .pipe(res);
-  } else if (endpoint === "critics") {
-    let proxy =
-      "3.16.200.137/" + path;
-    request(proxy)
-      .on("error", err => console.log(err))
-      .pipe(res);
-  }
-});
-
-app.get("/", (req, res) => {});
+app.use("/movie", proxy({
+  target: "18.217.66.137"
+}));
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Server listening on port " + port));
