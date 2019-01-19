9d603753882601033905a2d7ccb526cc5741ffc9
Jonathan Raitses <jonathan.raitses@gmail.com>
2019-01-19 16:38:52 -0500

    finished

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const request = require("request");
 const proxy = require("http-proxy-middleware");
 const app = express();
 
 app.use(express.static(__dirname));
 
-app.use("/movie", proxy({
-  target: "18.217.66.137"
-}));
+app.use(
+ "/movie",
+ proxy({
+   target: "http://18.217.66.137"
+ })
+);
 
-let port = process.env.PORT || 3000;
+let port = 3000 || process.env.PORT; //Switched around
 app.listen(port, () => console.log("Server listening on port " + port));
