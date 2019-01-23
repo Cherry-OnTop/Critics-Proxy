177c11bfa602f40bc25dc43d2411e2bc704daa29
Jonathan Raitses <jonathan.raitses@gmail.com>
2019-01-23 10:50:00 -0500

    deployed

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-const express = require("express");
-const request = require("request");
-const proxy = require("http-proxy-middleware");
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const proxy = require('http-proxy-middleware');
+
+
 const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+// '/:movieId', 
 
 app.use(express.static(__dirname));
 
-app.use(
- "/movie",
- proxy({
-   target: "http://18.217.66.137"
- })
-);
+app.use('/movie', proxy({
+    target: 'http://18.217.66.137'
+}));
 
-let port = 3000 || process.env.PORT; //Switched around
-app.listen(port, () => console.log("Server listening on port " + port));
+app.use('/movies', proxy({
+    target: 'http://54.146.26.33'
+}));
+
+app.use('/movies', proxy({
+    target: 'http://3.16.200.137'
+}))
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
